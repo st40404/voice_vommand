df3681f36a6c57c69b1ad99a996ce7fa26fd50c7
st40404 <lung40404@gmail.com>
2025-09-16 16:46:17 +0800

    add get_command.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'voice_command'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ron',
+    maintainer_email='ron@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'get_command = voice_command.1_get_command:main',
+        ],
+    },
+)
